a0e5c94afb004eb7f8c1654f90af4faedbb3577b
kranthi <kranthi.hemanth2010@gmail.com>
2018-03-19 01:35:17 +0530

    added material layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -83,33 +83,19 @@ export class MaterialModule {}
 
 
 
-// import { BrowserModule } from '@angular/platform-browser';
-// import {CdkTableModule} from '@angular/cdk/table';
-// import {HttpClientModule} from '@angular/common/http';
-// import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
-// import {HttpModule} from '@angular/http';
-// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
-
-// import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-
 import { AppComponent } from './app.component';
-import { ServerComponent} from './Server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
-import { PageHeaderComponent } from './page-header/page-header.component';
-import { PageDownloadComponent } from './page-download/page-download.component';
 import { PageFeaturesComponent } from './page-features/page-features.component';
-import { PageCtaComponent } from './page-cta/page-cta.component';
-import { PageContactComponent } from './page-contact/page-contact.component';
 import { PageFooterComponent } from './page-footer/page-footer.component'
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, MainNavComponent, PageHeaderComponent, PageDownloadComponent, PageFeaturesComponent, PageCtaComponent, PageContactComponent, PageFooterComponent],
+  declarations: [AppComponent, MainNavComponent, PageFeaturesComponent, PageFooterComponent],
   imports: [
-    MaterialModule,
+    MaterialModule,AngularFontAwesomeModule,
     NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/page-features/page-features.component.ts
+++ src/app/page-features/page-features.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {startWith} from 'rxjs/operators/startWith';
+import {map} from 'rxjs/operators/map';
+import { FormControl } from '@angular/forms';
+
+export class State {
+  constructor(public name: string) { }
+}
 
 @Component({
   selector: 'app-page-features',
@@ -7,9 +15,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageFeaturesComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
+  stateCtrl: FormControl;
+  filteredStates: Observable<any[]>;
+
+  states: State[] = [
+    {
+      name: 'Bangalore',
+    },
+    {
+      name: 'Electronic City',
+    },
+    {
+      name: 'Arkansas',
+    },
+    {
+      name: 'California',
+    },
+    {
+      name: 'Florida',
+    },
+    {
+      name: 'Texas',
+    }
+  ];
+
+  constructor() {
+    this.stateCtrl = new FormControl();
+    this.filteredStates = this.stateCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(state => state ? this.filterStates(state) : this.states.slice())
+      );
+  }
+
+  filterStates(name: string) {
+    return this.states.filter(state =>
+      state.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
+  }
+
+
 
 }
+
+
+
+
+
+
